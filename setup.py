f6248e4c731d93946f51f440aba334d34c13dc58
Saikiisdone <103962427+Saikikusuoisgood@users.noreply.github.com>
2024-03-14 18:43:39 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Haramlib',
+   version='1.0',
+   description='No more haram python coding',
+   author='saikiki',
+   author_email='',
+   packages=['Haramlib'],  #same as name
+   install_requires=[''], #external packages as dependencies
+)
